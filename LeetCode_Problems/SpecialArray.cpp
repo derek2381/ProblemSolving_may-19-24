d9275532444d005b0f8247a83504610f3689f368
derek2381 <satyakanthkarravula@gmail.com>
2024-05-19 20:08:08 +0530

    Today's Problem solving Practicse

---
+++ LeetCode_Problems/SpecialArray.cpp
@@ -0,0 +1,18 @@
+// problem link
+// https://leetcode.com/problems/special-array-i/description/
+
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        bool flag = true;
+
+        for(int i = 0;i < nums.size()-1;i++){
+            if((nums[i]%2 == 0 && nums[i+1]%2 == 0) || (nums[i])%2 == 1 && nums[i+1]%2 == 1){
+                flag = false;
+            }
+        }
+
+        return flag;
+
+    }
+};
